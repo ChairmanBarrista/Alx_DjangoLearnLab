5dcc948e6ee1464ff561f729af12366a81cc9e7b
Emmanuel Ofosu Amoah <barristaamoah@gmail.com>
2025-11-09 02:27:33 +0000

    1. Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book
+from .models import Library
 
 # Function-Based View (FBV) — List All Books
 def list_books(request):
@@ -13,5 +14,5 @@ def list_books(request):
 # Class-Based View (CBV) — Library Detail
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html'  # ✅ Namespaced path
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
