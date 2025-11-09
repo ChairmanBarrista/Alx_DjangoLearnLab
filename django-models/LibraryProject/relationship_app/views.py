38cdfb9570af3c685056ca063be21ee4dfb3bbe1
Emmanuel Ofosu Amoah <barristaamoah@gmail.com>
2025-11-09 21:51:07 +0000

    3. Implement Role-Based Access Control in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -74,8 +74,17 @@ def logout_view(request):
 
 
 # Functions to check user roles
+#def is_admin(user):
+#    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
 def is_admin(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+# Admin-only view
+@user_passes_test(is_admin)
+def admin_view(request):
+    """View accessible only to users with the 'Admin' role."""
+    return render(request, 'relationship_app/admin_view.html')
 
 def is_librarian(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
