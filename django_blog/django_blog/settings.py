ec23478bd3240913d4ec21bd52633cd3ca08812f
Emmanuel Ofosu Amoah <barristaamoah@gmail.com>
2025-12-06 22:56:55 +0000

    0. Initial Setup and Project Configuration for a Django Blog

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
         'USER': 'root',
-        'PASSWORD': 'password',
+        'PORT': '8080',
     }
 }
 
