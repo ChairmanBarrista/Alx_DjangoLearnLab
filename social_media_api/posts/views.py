0254e5e7733200bdad4fdabba1b09bb75c7b63a1
Emmanuel Ofosu Amoah <barristaamoah@gmail.com>
2025-12-14 23:09:24 +0000

    3. Implementing Notifications and Likes Functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -97,3 +97,4 @@ class UnlikePostView(APIView):
         return Response({"detail": "Post unliked."})
 
 #"Post.objects.filter(author__in=following_users).order_by", "permissions.IsAuthenticated"]
+["generics.get_object_or_404(Post, pk=pk)", "Like.objects.get_or_create(user=request.user, post=post)"]
